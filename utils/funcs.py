91434b08b1998c496f14658f206f0b2d354d47e7
arthishaxom <ashishpothal@gmail.com>
2024-09-05 16:07:11 +0530

    Update funcs.py

--- utils/funcs.py
+++ utils/funcs.py
@@ -1,36 +1,48 @@
 from __future__ import print_function
 import time
 import sib_api_v3_sdk
-from sib_api_v3_sdk.rest import ApiException
-from pprint import pprint
+import resend
+
 import config
 from datetime import datetime
 
 
 def sendOtp(emailAddress: str, otp: str):
-    configuration = sib_api_v3_sdk.Configuration()
-    configuration.api_key["api-key"] = config.BREVO_KEY
+    resend.api_key = "re_123456789"
 
-    api_instance = sib_api_v3_sdk.TransactionalEmailsApi(
-        sib_api_v3_sdk.ApiClient(configuration)
-    )
-    subject = "Verify KIIT HIVE OTP"
-    html_content = (
-        f"<html><body><p>Your OTP - <strong>{otp}</strong>. Valid for 5 minutes only. </p></body></html>"
-    )
-    sender = {"name": "KIIT HIVE", "email": "kiithive@ashishpothal.tech"}
-    to = [{"email": f"{emailAddress}"}]
-    send_smtp_email = sib_api_v3_sdk.SendSmtpEmail(
-        to=to,
-        html_content=html_content,
-        sender=sender,
-        subject=subject,
-    )
+    params: resend.Emails.SendParams = {
+    "from": "KIIT HIVE <kiithive@ashishpothal.tech>",
+    "to": [f"{emailAddress}"],
+    "subject": "Verify KIIT HIVE OTP",
+    "html": f"<html><body><p>Your OTP - <strong>{otp}</strong>. Valid for 5 minutes only. </p></body></html>"
+    }
+
+    
+    
+    # configuration = sib_api_v3_sdk.Configuration()
+    # configuration.api_key["api-key"] = config.BREVO_KEY
+
+    # api_instance = sib_api_v3_sdk.TransactionalEmailsApi(
+    #     sib_api_v3_sdk.ApiClient(configuration)
+    # )
+    # subject = "Verify KIIT HIVE OTP"
+    # html_content = (
+    #     f"<html><body><p>Your OTP - <strong>{otp}</strong>. Valid for 5 minutes only. </p></body></html>"
+    # )
+    # sender = {"name": "KIIT HIVE", "email": "kiithive@ashishpothal.tech"}
+    # to = [{"email": f"{emailAddress}"}]
+    # send_smtp_email = sib_api_v3_sdk.SendSmtpEmail(
+    #     to=to,
+    #     html_content=html_content,
+    #     sender=sender,
+    #     subject=subject,
+    # )
 
     try:
-        api_response = api_instance.send_transac_email(send_smtp_email)
+        # api_response = api_instance.send_transac_email(send_smtp_email)
         # pprint(api_response)
-    except ApiException as e:
+        email = resend.Emails.send(params)
+    except Exception as e:
         print("Exception when calling SMTPApi->send_transac_email: %s\n" % e)
         return "error"
 
